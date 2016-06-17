06cba60c38f2705ba6773bd3d8d7a563ee1d8504
DelphiRace <delphi.race@gmail.com>
2016-06-17 10:58:17 +0800

    update

--- config/router/config.php
+++ config/router/config.php
@@ -31,6 +31,16 @@ return array(
         //     'index' => 'POST'
         // )
     ),
+    "workflow" => array(
+        'controller' => 'workflow',
+        'action' => 'index',
+        // 針對全部呼叫方法
+        // 'method' => 'GET'
+        // 針對單一Action呼叫方法
+        // 'ActionMethod' => array(
+        //     'index' => 'POST'
+        // )
+    ),
 );
 
 ?>
